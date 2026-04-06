97fabeab426bbbf02a41b7f2f44eeb2f436c55bc
k1ng-bit <daksh310303@gmail.com>
2026-04-05 20:40:00 -0400

    Task 1 and Task 2
    Completed Task 1 and 2. Scraped app reviews and stored in raw review file. Created cleaning script and stored cleaned reviews in clean review data file.
    
    Created metadata file.

--- src/01_collect_or_import.py
+++ src/01_collect_or_import.py
@@ -1,1 +1,31 @@
 """imports or reads your raw dataset; if you scraped, include scraper here"""
+
+from google_play_scraper import reviews_all
+import json
+
+# the app id for MindDoc: Mental Health Support
+app_id = "de.moodpath.android"
+
+# fetching all the reviews
+reviews = reviews_all(
+    app_id,
+    lang='en',
+    country='us',       #taking reviews from US
+    sleep_milliseconds=0
+)
+
+reviews = reviews[:3500] #taking 3500 reviews
+
+print("Number of Reviews taken: " , len(reviews))
+print(reviews[0])       #printing the first review
+
+with open("../data/reviews_raw.jsonl", "w", encoding="utf-8") as f:     #writing the raw reviews into reviews_raw.jsonl file
+    for i, review in enumerate(reviews):
+        json.dump({
+            "review_id": i,
+            "content": review["content"],
+            "score": review["score"]
+        }, f)
+        f.write("\n")
+
+print("\n Updated reviews_raw.jsonl, Data saved ! \n\n")
--- src/02_clean.py
+++ src/02_clean.py
@@ -1,1 +1,39 @@
 """cleans raw data & make clean dataset"""
+
+#clean reviews
+
+import json
+import re
+
+def clean_text(text):
+    text = text.lower()                         # change text to lowercase
+    text = re.sub(r'[^a-z\s]', '', text)        # special characters
+    text = re.sub(r'\s+', ' ', text).strip()    # whitespaces
+    return text
+
+seen = set()
+cleaned = []
+
+with open('../data/reviews_raw.jsonl', 'r', encoding='utf-8') as f:
+    for line in f:
+        data = json.loads(line)
+        content = clean_text(data["content"])
+
+        if len(content.split()) < 3:
+            continue
+        if content in seen:
+            continue
+
+        seen.add(content)
+
+        cleaned.append({
+            "review_id": data["review_id"],
+            "content": content
+        })
+
+with open('../data/reviews_clean.jsonl', 'w', encoding='utf-8') as f:        #write cleaned reviews to reviews_clean.jsonl
+    for r in cleaned:
+        json.dump(r, f)
+        f.write("\n")
+
+print(f" \n Updated reviews_clean.jsonl, cleaned dataset size: {len(cleaned)} \n\n")
